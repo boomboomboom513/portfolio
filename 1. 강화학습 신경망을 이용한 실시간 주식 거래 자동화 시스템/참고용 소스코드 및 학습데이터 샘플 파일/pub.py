60247149cee070093bda325097f31b19787a9249
boomboomboom513 <boomboomboom513@gmail.com>
2023-07-25 23:20:33 +0900

    포트폴리오 - 마지막 작성(2023/07/25)

--- 1. 강화학습 신경망을 이용한 실시간 주식 거래 자동화 시스템/참고용 소스코드 및 학습데이터 샘플 파일/pub.py
+++
@@ -1,9 +0,0 @@
-import redis
-import pandas as pd
-
-address = redis.Redis(host='localhost', port=6388, db=0)
-
-dataDf = pd.read_csv('./32bit/hapminstockdata/데이타솔루션 분 단위 주식 데이터.csv', encoding='euc-kr')
-
-address.set('column', str(dataDf.columns.values))
-address.set('value', str(dataDf[-1:].values))
